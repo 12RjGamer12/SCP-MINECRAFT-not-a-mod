d31aa9b2ce492c4fda0ce57847dfea2c2d50fb82
12RjGamer12 <102541014+12RjGamer12@users.noreply.github.com>
2022-05-28 17:27:49 -0400

    Update 1.0

---
+++ SCP-DATA BASE1.py
@@ -0,0 +1,81 @@
+#WARNING: THIS CODE IS HIGHLY CLASSAFIED: ANY UNAUTHRIZED VIEWING WILL RESULT IN TERMANATION
+from time import sleep
+
+print ("USERS AUTHRIZED TO VIEW THIS TERMANALE INCLUDE")
+
+sleep(1)
+
+print("12_RjGamer_12, Cloud_Maan, FakeCoolMann, 0Drugstinve0, True_Explorer.")
+
+sleep(5)
+
+access = input("ANY UNAUTHRIZED VIEWING WILL RESULT IN TERMANATION. ACCESS DATA BASE? IF YES, THEN PRESS [ENTER] IF NO PRESS [CTRL] + [C]\n")
+
+sleep(0.50)
+
+#WARNING ANY UNAUTHRIZED VIEWING OF THE CONTENT BELOW WILL RESULT IN AN END PRISION FOR 24 HOURS.
+print ("Enter credentials")
+
+psswd = input ("")
+
+if psswd == "a7Xtwc":
+
+    print("ACCESS GRANTED")
+    
+    print("ALL FILES")
+    sleep(0.05)
+    print("Base Locations, Newest Operations, How to protect our bases, Endermen Termanaiton Atempt, Update Log. ")
+#Here is where i need to write stuff v
+    ask = input("Coppy phast above: ")
+
+    if ask == "Update Log":
+        sleep(0.05)
+        print("Update Log:")
+        sleep(0.05)
+        print("Added Update Log, Curentley working on another program, Fixed Typo Nothing else.")
+        wait = input("Press Enter to close this window.")
+        
+
+    if ask == "Endermen Termanation Atempt":
+        sleep(0.05)
+        print("Endermen Termnation Atempt")
+        sleep(0.05)
+        print("Endermen Termanation atempt was an atempt to get rid of all the ender men in the facilitys radius: ESTIMATED SIZE: 100x70 Meters. Atempt #1 Sucsefull")
+        wait = input ("Press enter to close the program")
+        exit()
+
+    if ask == "How to protect our bases":
+        sleep(0.05)
+        print("How to protect our bases")
+        sleep(0.05)
+        print ("How to protect our bases: Idea #1: Make them down low, or high in the sky. Idea #2: Have personle At the base at all times. Idea #3: Don't anoy Miath. Idea #4: Add defences.")
+        wait = input("Press Enter to close ")
+        exit()
+
+               
+    if ask == "Newest Operations":
+        sleep(0.05)
+        print("Opeartion, Save New Players from Miath:")
+        sleep(0.05)
+        print("Operation SNPM (SAVE PLAYERS FROM MIATH) Is an operation to save all the new players from the 500 metere block trench, by putting a buch of nether portales evreywhere at spawn, whil miath is not online. ")
+        wait = input("Press Enter To close")
+        exit()
+        
+
+    if ask == "Base Locations":
+        sleep(0.05)
+        print("ALL BASE CORDS:")
+        sleep(0.05)
+        print("SITE-21: DIMENSION, OVER WORLD, CORDS: -20,842 61 -2,121, NETHER CORDS: -178 36. STATUS: NUKED.")
+        sleep(0.05)
+        print("SITE-15: DIMENSION, (END). CORDS: 14985 57 10062, STATUS: NUKED.")
+        wait = input("Press Enter To close")
+        exit()
+
+
+      
+else:
+    print("ACCESS DENIED")
+    sleep(2)
+    exit()
+
